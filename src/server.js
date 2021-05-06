c66875180b036c4d8827d3736d3144571e168204
Ivan Sbrolla <ivansbrolla1@gmail.com>
2021-05-06 00:14:15 -0300

    Implementado getAddress

--- src/server.js
+++ src/server.js
@@ -1,9 +1,16 @@
-const bodyParser = require('body-parser');
 const express = require('express');
+const axios = require('axios');
 const app = express();
 
 app.listen(8080, () => console.log('server ON!'));
 
 app.use(express.static('./.'));
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+
+app.get('/ConsultCep',(req,res) => {
+    const cep = req.query.cep;
+    getAddress(cep).then(x => res.send(x.data));
+    })
+
+async function getAddress(cep){
+    return await axios.get(`https://viacep.com.br/ws/${cep}/json/unicode/`)
+}
